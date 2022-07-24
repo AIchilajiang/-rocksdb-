7d105c7480f0794a47694e1aa475311b3e5a43a9
AIchilajiang <63706515+AIchilajiang@users.noreply.github.com>
2022-07-24 17:04:50 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,98 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include "rocksdb/db.h"
+#include "rocksdb/slice.h"
+#include "rocksdb/options.h"
+using namespace std;
+using namespace rocksdb;
+const std::string PATH = "/tmp/rocksdbResult";
+class Graph{
+    public:
+        DB* db;
+        Options options;
+        Graph()//构造函数
+        {      //打开数据库
+            options.create_if_missing = true;
+            rocksdb::Status status = rocksdb::DB::Open(options, PATH, &db);
+            assert(status.ok());
+        }
+        void closeGraph()//关闭数据库
+        {
+            delete db;
+        }
+        void add_node(std::string pk,std::string property)
+        {
+            std::string propertys = "";
+            std::string key = "",value = "";
+            int flag = 0;
+            for(int i = 0; i < property.length(); i++)
+            {
+                if(property[i] != '=' && flag == 0) key += property[i];
+                if(property[i] == '=') flag = 1;
+                if(property[i] != ',' && flag == 1) value += property[i];
+                if(property[i] == ',' || i == property.length()-1){
+                    propertys += key + ",";
+                    std::string pk_key = pk + "_" + key;
+                    rocksdb::Status s = db -> Put(rocksdb::WriteOptions(),pk_key,value);
+                    if(s.ok()) {
+                        flag = 0;
+                        key = "";
+                        value = "";
+                    }
+                }
+            }
+            rocksdb::Status s = db -> Put(rocksdb::WriteOptions(),pk + "_type","node");
+            propertys += "type;";
+            if(s.ok())  s = db -> Put(rocksdb::WriteOptions(),pk,propertys);
+        }
+        void add_edge(std::string pk,std::string property)
+        {
+            std::string propertys = "";
+            std::string key = "",value = "";
+            int flag = 0;
+            for(int i = 0; i < property.length(); i++)
+            {
+                if(property[i] != '=' && flag == 0) key += property[i];
+                if(property[i] == '=') flag = 1;
+                if(property[i] != ',' && flag == 1) value += property[i];
+                if(property[i] == ',' || i == property.length()-1){
+                    propertys += key + ",";
+                    std::string pk_key = pk + "_" + key;
+                    rocksdb::Status s = db -> Put(rocksdb::WriteOptions(),pk_key,value);
+                    if(s.ok()) {
+                        flag = 0;
+                        key = "";
+                        value = "";
+                    }
+                }
+            }
+            rocksdb::Status s = db -> Put(rocksdb::WriteOptions(),pk + "_type","edge");
+            propertys += "type;";
+            if(s.ok())  s = db -> Put(rocksdb::WriteOptions(),pk,propertys);
+        }
+        void update(std::string pk,std::string value)
+        {
+
+        }
+        void delete_node(std::string pk)
+        {
+
+        }
+        void delete_edge(std::string pk)
+        {
+
+        }
+};
+
+
+int main(){
+    Graph g = Graph();
+    g.add_node("wsc","name=wsc,age=15,address=china");
+    g.add_edge("win","date=22.4.14");
+    std::string v;
+    rocksdb::Status s = g.db -> Get(rocksdb::ReadOptions(),"win",&v);
+    if(s.ok()) cout << "win:   " << v << endl;
+    g.closeGraph();
+}
